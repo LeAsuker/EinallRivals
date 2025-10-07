a87cd0c4b760a8b38d599d1e43a99df792ecdac7
Asuker <michalek.jonas@proton.me>
2025-10-07 12:46:46 +0200

    Moved util functions to utils.c

--- src/main.c
+++ src/main.c
@@ -169,26 +169,6 @@ int main(void) {
   return 0;
 }
 
-int safe_mouse_x(GridConfig * grid_config) {
-  int mouse_pos = GetMouseX();
-  if (mouse_pos >= grid_config->grid_offset_x + grid_config->max_grid_cells_x* grid_config->grid_cell_size) {
-    return grid_config->grid_offset_x + grid_config->max_grid_cells_x * grid_config->grid_cell_size - 1;
-  } else if (mouse_pos <= grid_config->grid_offset_x) {
-    return grid_config->grid_offset_x + 1;
-  }
-  return mouse_pos;
-}
-
-int safe_mouse_y(GridConfig * grid_config) {
-  int mouse_pos = GetMouseY();
-  if (mouse_pos >= grid_config->grid_offset_y + grid_config->max_grid_cells_y * grid_config->grid_cell_size) {
-    return grid_config->grid_offset_y + grid_config->max_grid_cells_y * grid_config->grid_cell_size - 1;
-  } else if (mouse_pos <= grid_config->grid_offset_y) {
-    return grid_config->grid_offset_y + 1;
-  }
-  return mouse_pos;
-}
-
 Point *mouseToCell(GridConfig * grid_config, Point *point_arr) {
   // will need to sanitize it so it cant ever go out of bounds, else undefined
   int x = (safe_mouse_x(grid_config) - grid_config->grid_offset_x) / grid_config->grid_cell_size;
---
+++ src/utils.c
@@ -0,0 +1,21 @@
+#include "utils.h"
+
+int safe_mouse_x(GridConfig * grid_config) {
+  int mouse_pos = GetMouseX();
+  if (mouse_pos >= grid_config->grid_offset_x + grid_config->max_grid_cells_x* grid_config->grid_cell_size) {
+    return grid_config->grid_offset_x + grid_config->max_grid_cells_x * grid_config->grid_cell_size - 1;
+  } else if (mouse_pos <= grid_config->grid_offset_x) {
+    return grid_config->grid_offset_x + 1;
+  }
+  return mouse_pos;
+}
+
+int safe_mouse_y(GridConfig * grid_config) {
+  int mouse_pos = GetMouseY();
+  if (mouse_pos >= grid_config->grid_offset_y + grid_config->max_grid_cells_y * grid_config->grid_cell_size) {
+    return grid_config->grid_offset_y + grid_config->max_grid_cells_y * grid_config->grid_cell_size - 1;
+  } else if (mouse_pos <= grid_config->grid_offset_y) {
+    return grid_config->grid_offset_y + 1;
+  }
+  return mouse_pos;
+}
