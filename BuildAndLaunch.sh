acc7fdaed40f3172f6e5bdba072b99d42a67359f
Asuker <michalek.jonas@proton.me>
2025-12-14 12:31:58 +0100

    Added macos support

--- BuildAndLaunch.sh
+++ BuildAndLaunch.sh
@@ -1,6 +1,25 @@
-cd build
-./premake5 gmake
-cd ..
-make
-cd ./bin/Debug
-./EinallRivals
+if [ "$#" -ne 1 ] || [ "$1" != "linux" ] && [ "$1" != "macos" ]; then
+    echo "USAGE: ./BuildAndLaunch.sh <linux/macos>"
+    exit 1
+fi
+
+if [ "$1" = "linux" ]; then
+    cd build
+    ./premake5 gmake
+    cd ..
+    make
+    cd ./bin/Debug
+    ./EinallRivals
+    exit 0
+fi
+
+if [ "$1" = "macos" ]; then
+    cd build
+    ./premake5.osx gmake
+    cd ..
+    make
+    cd ./bin/Debug
+    ./EinallRivals
+    exit 0
+fi
+
